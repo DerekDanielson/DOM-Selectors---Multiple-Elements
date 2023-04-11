d7f997793f5c8226f9530c266ca288bb9efe984d
Derek <derekdanielson84@gmail.com>
2023-04-11 10:56:30 -0600

    Notes added

---
+++ app.js
@@ -0,0 +1,30 @@
+// querySelectorAll()
+
+const listItems = document.querySelectorAll('.item');
+console.log(listItems[1].innerText);
+
+listItems[1].getElementsByClassName.color = 'red';
+
+listItems.forEach((item) => {            //turn each li red
+    item.style.color = 'red';
+
+    if (index === 1) {
+        item.remove();
+    }
+    if (index === 0) {
+        item.innerText = 'Oranges';
+    }
+});
+
+
+const listItems2 = document.getElementsByClassName('item');
+
+console.log(listItems2[2].innerText);
+
+const listItemsArray = Array.from(listItems2);
+
+listItemsArray.forEach((item) => {
+    console.log(item.innerText);
+});
+
+
